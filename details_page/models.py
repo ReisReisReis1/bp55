7db7f5f2f844e08a0569d065988cfdbc6334b49a
CottlestonPie1 <lbmail55@gmail.com>
2021-01-13 12:38:51 +0100

    added missing functions to models and view
    added functions to views and missing getters to models. videos, pictures and building_plan still needs to be implemented

--- details_page/models.py
+++ details_page/models.py
@@ -4,7 +4,7 @@ Configurations for the Database Models for the App 'details_page'
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.db.models import CharField, Value as V
-from django.db.models.functio
+from django.db.models.functios import Concat
 
 class Detail(models.model):
     """
@@ -31,15 +31,16 @@ class Detail(models.model):
     literature: further literature about the building
     era: era in which the building was built
     videos: videos about the building
-    pictures: pictures of teh building
+    pictures: pictures of the building
     building_plan: building plan of the building
+    timestamps:
     """
     name = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     region = models.CharField(max_length=100)
     country = models.CharField(max_length=5)
-    date_from: models.DateField()
-    date_to: models.DateField()
+    date_from: models.IntegerField()
+    date_to: models.IntegerField()
     architect = models.CharField(max_length=100)
     context = models.CharField(max_length=100)
     builder = models.CharField(max_length=100)
@@ -73,17 +74,14 @@ class Detail(models.model):
         """
         return self.name
 
-    def get_city(self):
+    def get_city_region(self):
         """
         :return: city in which the building is located
         """
+        city_variable= self.city
+        region_variable= self.region
+        city_and_region= Concat('city' , V('/'), 'region')
         return self.city
-
-    def get_region(self):
-        """
-        :return: region in which the building is located
-        """
-        return self.region
 
     def get_country(self):
         """
@@ -95,7 +93,18 @@ class Detail(models.model):
         """
         :return: date
         """
-
+        date_to_string= srting(self.date_to)
+        date_from_string=string(self.date_from)
+        if self.date_to<0:
+            date_to_with_BCAD=Concat('date_to_string', V('BC'))
+        else:
+            date_to_with_BCAD=Concat('date_to_string', V('AD'))
+        if self.date_from<0:
+            date_from_with_BCAD=Concat('date_from_string', V('BC'))
+        else:
+            date_from_with_BCAD=Concat('date_from_string', V('AD'))
+        date_total=Concat('date_from_with_BCAD', V('-'), 'date_to_with_BCAD')
+        return date_total
 
     def get_architect(self):
         """
@@ -133,7 +142,61 @@ class Detail(models.model):
         """
         return self.function
 
-    def get_
+    def get_dimension(self):
+        """
+        :return: Dimension of the building including length, width, height, circumference and area
+        """
+        length_var=self.length
+        width_var=self.width
+        height_var=self.height
+        circumference_var=self.circumference
+        area_var=self.area
+        dimension= Concat(V('Länge:'), 'length_var', V('Breite:'), 'width_var', V('Höhe:'), 'height_var', V('Durchmesser'), 'circumference_var', V('Fläche:'), 'area_var')
+        return dimension
+
+    def get_column_order(self):
+        """
+        :return: column order of the building
+        """
+        return self.column_order
+
+    def get_construction(self):
+        """
+        :return: construction of the building
+        """
+        return self.construction
+
+    def get_material(self):
+        """
+        :return: material of the building
+        """
+        return self.material
+
+    def get_literature(self):
+        """
+        :return: further literature about the building
+        """
+        return self.literature
+
+    def get_videos(self):
+        """
+        :return: videos about the building
+        """
+        return self.videos
+
+    def get_pictures(self):
+        """
+        :return: pictures of the building
+        """
+        return self.pictures
+
+    def get_building_plan(self):
+        """
+        :return: building plan of the building
+        """
+        return self.building_plan
+
+
 
 
 
--- details_page/views.py
+++ details_page/views.py
@@ -3,13 +3,35 @@ Configurations of the different viewable functions and subpages from the App: de
 """
 
 from django.shortcuts import render
+# pylint: disable = import-error, relative beyond-top-level
+from .models import Detail
 
 
 def detailed(request):
     """
-    Subpage to details_page
+    Subpage to show the characteristics of a building
     :param request: url request to get details_page
     :return: rendering the subpage based on detailed.html
+    with a context variable to get the characteristics
     """
+    context = {
+        'Name' : Detail.get_name(Detail),
+        'Ort/Region' : Detail.get_city_region(Detail),
+        'Land' : Detail.get_date(Detail),
+        'Architekt' : Detail.get_architect(Detail),
+        'Kontext/Lage' : Detail.get_context(Detail),
+        'Bauherr' : Detail.get_builder(Detail),
+        'Bautypus' : Detail.get_construction_type(Detail),
+        'Bauform' : Detail.get_design(Detail),
+        'Gattung/Funktion' :  Detail.get_function(Detail),
+        'Dimension' : Detail.get_dimension(Detail),
+        'Säulenordung' : Detail.get_column_order(Detail),
+        'Konstruktion' : Detail.get_construction(Detail),
+        'Material' : Detail.get_material(Detail),
+        'Litertur' : Detail.get_literature(Detail),
+        'Videos' : Detail.get_videos(Detail),
+        'Bilder' : Detail.get_Bilder(Detail),
+        'Baupläne' : Detail.get_building_plan(Detail),
+    }
 
-    return render(request, 'detailed.html')
+    return render(request, 'detailed.html', context)
