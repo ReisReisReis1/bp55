9b8aff81cddfb2ad142b5d00081374efd947a919
CottlestonPie1 <lbmail55@gmail.com>
2021-01-14 11:57:35 +0100

    Update models.py
    Added the getter functions

--- details_page/models.py
+++ details_page/models.py
@@ -140,3 +140,132 @@ class Picture(models.Model):
     def __str__(self):
         return self.name
 
+    def get_name(self):
+        """
+        :return: name of the building
+        """
+        return self.name
+
+    def get_city_region(self):
+        """
+        :return: city in which the building is located
+        """
+        city_variable= self.city
+        region_variable= self.region
+        city_and_region= Concat('city' , V('/'), 'region')
+        return self.city
+
+    def get_country(self):
+        """
+        :return: country in which the building is located
+        """
+        return self.country
+
+    def get_date(self):
+        """
+        :return: date
+        """
+        date_to_string= srting(self.date_to)
+        date_from_string=string(self.date_from)
+        if self.date_to<0:
+            date_to_with_BCAD=Concat('date_to_string', V('BC'))
+        else:
+            date_to_with_BCAD=Concat('date_to_string', V('AD'))
+        if self.date_from<0:
+            date_from_with_BCAD=Concat('date_from_string', V('BC'))
+        else:
+            date_from_with_BCAD=Concat('date_from_string', V('AD'))
+        date_total=Concat('date_from_with_BCAD', V('-'), 'date_to_with_BCAD')
+        return date_total
+
+    def get_architect(self):
+        """
+        :return: architect of the building
+        """
+        return self.architect
+
+    def get_context(self):
+        """
+        :return: context/type of the building
+        """
+        return self.context
+
+    def get_builder(self):
+        """
+        :return: builder of the building
+        """
+        return self.builder
+
+    def get_construction_type(self):
+        """
+        :return: construction type of the building
+        """
+        return self.construction
+
+    def get_design(self):
+        """
+        :return: design/shape of the building
+        """
+        return self.design
+
+    def get_funtion(self):
+        """
+        :return: function of the building
+        """
+        return self.function
+
+    def get_dimension(self):
+        """
+        :return: Dimension of the building including length, width, height, circumference and area
+        """
+        length_var=self.length
+        width_var=self.width
+        height_var=self.height
+        circumference_var=self.circumference
+        area_var=self.area
+        dimension= Concat(V('Länge:'), 'length_var', V('Breite:'), 'width_var', V('Höhe:'), 'height_var', V('Durchmesser'), 'circumference_var', V('Fläche:'), 'area_var')
+        return dimension
+
+    def get_column_order(self):
+        """
+        :return: column order of the building
+        """
+        return self.column_order
+
+    def get_construction(self):
+        """
+        :return: construction of the building
+        """
+        return self.construction
+
+    def get_material(self):
+        """
+        :return: material of the building
+        """
+        return self.material
+
+    def get_literature(self):
+        """
+        :return: further literature about the building
+        """
+        return self.literature
+
+    def get_videos(self):
+        """
+        :return: videos about the building
+        """
+        return self.videos
+
+    def get_pictures(self):
+        """
+        :return: pictures of the building
+        """
+        return self.pictures
+
+    def get_building_plan(self):
+        """
+        :return: building plan of the building
+        """
+        return self.building_plan
+
+
