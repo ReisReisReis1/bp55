6b319435434bfb7001207a6f5bf88dbc4c441853
CottlestonPie1 <lbmail55@gmail.com>
2021-01-14 11:55:27 +0100

    Revert "Revert "Merge branch 'dev_jonathan' into dev_laura""
    This reverts commit cacb81ac29ae5b926f320d3af834f27011713c79.

--- timeline/views.py
+++ timeline/views.py
@@ -4,6 +4,7 @@ Configurations of the different viewable functions and subpages from the App: ti
 
 
 from django.shortcuts import render
+from details_page import models
 
 # Create your views here.
 
