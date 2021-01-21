5dd3e3ac0714fdbcb506adfd92798e170f3ff2c7
Jonathan Otto <jonathan.otto.37@stud.tu-darmstadt.de>
2021-01-21 18:50:48 +0100

    Added filter logic. Updated .gitignore.

--- search/views.py
+++ search/views.py
@@ -15,22 +15,21 @@ def search(request):
              then return the rendered search.html with the search results
              else return nothing
     """
-    if request.method == 'POST':
-        search_id = request.POST.get('textfield', None)
-        results = Building.objects.filter(Building, Q(name__icontains=search_id) |
-                                          Q(city__icontains=search_id) | \
-                                          Q(region__icontains=search_id) | \
-                                          Q(country__icontains=search_id) | \
-                                          Q(era__name__icontains=search_id) | \
-                                          Q(architect__icontains=search_id) | \
-                                          Q(context__iscontains=search_id) | \
-                                          Q(builder__iscontains=search_id) | \
-                                          Q(construction_type=search_id) | \
-                                          Q(design__icontains=search_id) | \
-                                          Q(function__icontains=search_id) | \
-                                          Q(column_order__iscontains=search_id))
-        results = get_thumbnails_for_buildings(results)
-        context = {
-            'Result': results
-        }
-        return render(request, 'search.html', context)
+    search_id = request.POST.get('textfield', None)
+    results = Building.objects.filter(Building, Q(name__icontains=search_id) |
+                                      Q(city__icontains=search_id) | \
+                                      Q(region__icontains=search_id) | \
+                                      Q(country__icontains=search_id) | \
+                                      Q(era__name__icontains=search_id) | \
+                                      Q(architect__icontains=search_id) | \
+                                      Q(context__iscontains=search_id) | \
+                                      Q(builder__iscontains=search_id) | \
+                                      Q(construction_type=search_id) | \
+                                      Q(design__icontains=search_id) | \
+                                      Q(function__icontains=search_id) | \
+                                      Q(column_order__iscontains=search_id))
+    results = get_thumbnails_for_buildings(results)
+    context = {
+        'Result': results
+    }
+    return render(request, 'search.html', context)
