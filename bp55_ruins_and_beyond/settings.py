6717e461591027f1f0fc88d51ee4cd75ab2b20c0
Jonathan Otto <jonathan.otto.37@stud.tu-darmstadt.de>
2021-03-11 12:33:13 +0100

    Updated settings.py

--- bp55_ruins_and_beyond/settings.py
+++ bp55_ruins_and_beyond/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', "") != 'False'
 
-ALLOWED_HOSTS = ["dmz-48.architektur.tu-darmstadt.de", '127.0.0.1']
+ALLOWED_HOSTS = ["ruinsandbeyond.architektur.tu-darmstadt.de", '127.0.0.1']
 
 
 # Application definition
