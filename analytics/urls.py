c62bc61c7d62d0f18c18e06d3e8501f148800d33
Jonathan Otto <jonathan.otto.37@stud.tu-darmstadt.de>
2022-02-04 17:34:25 +0100

    Added analytics for frontend-only visits. Added ability to delete older analytics data.

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,7 +1,9 @@
-from django.urls import include, path
-from .views import analytics_view
+from django.urls import path
+from .views import analytics_view, add_visit, delete_old
 
 app_name = "analytics"
 urlpatterns = [
-    path('', analytics_view, name='analytics')
+    path('', analytics_view, name='analytics'),
+    path('add/<str:page>/<int:object_id>/', add_visit, name='add_visit'),
+    path('delete/', delete_old, name="delete_old")
 ]
